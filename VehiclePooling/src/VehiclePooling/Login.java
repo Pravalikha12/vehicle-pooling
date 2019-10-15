56e4568235dd7b4d97777379b5443b5eaf744a7a
pravalikha <43210968+pravalikha@users.noreply.github.com>
2019-10-15 16:38:59 +0530

    Added Login Page

---
+++ VehiclePooling/src/VehiclePooling/Login.java
@@ -0,0 +1,87 @@
+package VehiclePooling;
+
+import java.sql.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField name;
+	private JPasswordField passwd;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		name = new JTextField();
+		name.setBounds(192, 39, 144, 20);
+		contentPane.add(name);
+		name.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Username");
+		lblNewLabel.setBounds(10, 42, 172, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(10, 67, 144, 14);
+		contentPane.add(lblPassword);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				try{
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/VehiclePoolingDb","root","");
+					Statement stmt=con.createStatement();
+					String sql="Select * from user where name='"+name.getText()+"' and passwd='"+passwd.getText().toString()+"'";
+					ResultSet rs=stmt.executeQuery(sql);
+					if(rs.next())
+						JOptionPane.showMessageDialog(null,"Login Successfully...");
+					else
+						JOptionPane.showMessageDialog(null, "Incorrect password");
+					con.close();
+				} catch(Exception e) {System.out.println(e);}
+			}
+		});
+		btnLogin.setBounds(206, 130, 89, 23);
+		contentPane.add(btnLogin);
+		
+		passwd = new JPasswordField();
+		passwd.setBounds(192, 76, 144, 20);
+		contentPane.add(passwd);
+	}
+}
