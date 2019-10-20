5b8086d3177103f261b6a0c4cf2b5dd077b48156
pravalikha <pravalikha12@gmail.com>
2019-10-20 11:12:41 +0530

    Login page updated

--- src/components/Login.java
+++ src/components/Login.java
@@ -1,36 +1,281 @@
 package components;
 
+import java.sql.*;
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.*;
+
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JSplitPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
+import java.awt.Component;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import java.awt.ComponentOrientation;
+import java.awt.Cursor;
+import java.awt.Insets;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.BoxLayout;
+import java.awt.Panel;
+import java.awt.Rectangle;
+import java.awt.TextField;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JLayeredPane;
+import javax.swing.Box;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Label;
+import java.awt.Window.Type;
 
+public class Login extends JFrame {
+	private JTextField name;
+	private JPasswordField passwd;
+	private JTextField txtAllRightsReserved;
+	private JTextField txtAboutUs;
+	private JTextField txtImage;
+	private JTextField txtName;
+	private JTextField txtPassword;
 
-public class Login {
-	private static JTextField txtAbout;    // Your class name
-	private static JTextField txtSignIn;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-  public static void main(String[] args) {
-     
-     JFrame f = new JFrame("A JFrame");
-     f.setSize(250, 250);
-     f.setLocation(300,200);
-     f.getContentPane().add(BorderLayout.CENTER, new JTextArea(10, 40));
-     
-     JSplitPane splitPane = new JSplitPane();
-     f.getContentPane().add(splitPane, BorderLayout.WEST);
-     
-     txtAbout = new JTextField();
-     txtAbout.setText("ABOUT");
-     splitPane.setLeftComponent(txtAbout);
-     txtAbout.setColumns(10);
-     
-     txtSignIn = new JTextField();
-     txtSignIn.setText("Sign In");
-     splitPane.setRightComponent(txtSignIn);
-     txtSignIn.setColumns(10);
-     f.setVisible(true);
-     
-   }
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setType(Type.UTILITY);
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		
+		JPanel panel_2 = new JPanel();
+		
+		JPanel panel_3 = new JPanel();
+		
+		txtAboutUs = new JTextField();
+		txtAboutUs.setText("About us");
+		txtAboutUs.setColumns(10);
+		
+		txtImage = new JTextField();
+		txtImage.setText("Image");
+		txtImage.setColumns(10);
+		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
+		gl_panel_3.setHorizontalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_3.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtImage, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtAboutUs, GroupLayout.DEFAULT_SIZE, 402, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_panel_3.setVerticalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(txtAboutUs, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(txtImage, GroupLayout.DEFAULT_SIZE, 79, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		panel_3.setLayout(gl_panel_3);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 449, Short.MAX_VALUE)
+						.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+							.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 213, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(tabbedPane, 0, 0, Short.MAX_VALUE))
+							.addComponent(panel_2, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 426, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE)
+							.addGap(18))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(tabbedPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		
+		JPanel panel_4 = new JPanel();
+		tabbedPane.addTab("Sign Up", null, panel_4, null);
+		
+		JLabel label = new JLabel("");
+		
+		JLabel lblRegistrations = new JLabel("Registration page");
+		GroupLayout gl_panel_4 = new GroupLayout(panel_4);
+		gl_panel_4.setHorizontalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_4.createSequentialGroup()
+					.addGroup(gl_panel_4.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_4.createSequentialGroup()
+							.addGap(112)
+							.addComponent(label))
+						.addGroup(gl_panel_4.createSequentialGroup()
+							.addGap(30)
+							.addComponent(lblRegistrations)))
+					.addContainerGap(113, Short.MAX_VALUE))
+		);
+		gl_panel_4.setVerticalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_4.createSequentialGroup()
+					.addGap(5)
+					.addComponent(label)
+					.addGap(38)
+					.addComponent(lblRegistrations)
+					.addContainerGap(62, Short.MAX_VALUE))
+		);
+		panel_4.setLayout(gl_panel_4);
+		
+		JPanel panel_5 = new JPanel();
+		tabbedPane.addTab("Sign In", null, panel_5, null);
+		
+		JLabel lblUserName = new JLabel("User Name");
+		
+		JLabel lblPassword = new JLabel("Password");
+		
+		txtName = new JTextField();
+		txtName.setText("name");
+		txtName.setColumns(10);
+		
+		txtPassword = new JTextField();
+		txtPassword.setText("password");
+		txtPassword.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		GroupLayout gl_panel_5 = new GroupLayout(panel_5);
+		gl_panel_5.setHorizontalGroup(
+			gl_panel_5.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_5.createSequentialGroup()
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblUserName)
+								.addGroup(gl_panel_5.createSequentialGroup()
+									.addComponent(lblPassword)
+									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addGap(48)
+							.addComponent(btnLogin)))
+					.addContainerGap())
+				.addGroup(Alignment.TRAILING, gl_panel_5.createSequentialGroup()
+					.addContainerGap(59, Short.MAX_VALUE)
+					.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(30))
+		);
+		gl_panel_5.setVerticalGroup(
+			gl_panel_5.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_5.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblUserName)
+					.addGap(1)
+					.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addGap(7)
+							.addComponent(lblPassword))
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addGap(18)
+							.addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(btnLogin))
+		);
+		panel_5.setLayout(gl_panel_5);
+		
+		txtAllRightsReserved = new JTextField();
+		txtAllRightsReserved.setText("All Rights Reserved 2019");
+		txtAllRightsReserved.setColumns(10);
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(txtAllRightsReserved, GroupLayout.DEFAULT_SIZE, 402, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(txtAllRightsReserved, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+		);
+		panel_2.setLayout(gl_panel_2);
+		
+		JLabel lblNammaRide = new JLabel("Namma Ride");
+		lblNammaRide.setFont(new Font("Liberation Serif", Font.BOLD | Font.ITALIC, 26));
+		lblNammaRide.setForeground(Color.BLACK);
+		lblNammaRide.setBackground(Color.RED);
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(21)
+					.addComponent(lblNammaRide, GroupLayout.PREFERRED_SIZE, 176, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(229, Short.MAX_VALUE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNammaRide, GroupLayout.DEFAULT_SIZE, 21, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		panel_1.setLayout(gl_panel_1);
+		panel.setLayout(gl_panel);
+	}
 }
