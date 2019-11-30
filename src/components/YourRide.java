76d358f58b63ead0ecb2d422dce667af10617939
PrateekaBhat <prateekabhat22@gmail.com>
2019-11-30 12:54:41 +0530

    Sign Up page modified

--- src/components/YourRide.java
+++ src/components/YourRide.java
@@ -1,12 +1,12 @@
 package components;
 
-import com.bbn.openmap.*;
+//import com.bbn.openmap.*;
 import java.awt.BorderLayout;
 import java.util.Properties;
 
 import javax.swing.JPanel;
-import com.bbn.openmap.MapBean;
-import com.bbn.openmap.layer.shape.ShapeLayer;
+//import com.bbn.openmap.MapBean;
+//import com.bbn.openmap.layer.shape.ShapeLayer;
 
 
 public class YourRide extends JPanel {
@@ -15,23 +15,23 @@ public class YourRide extends JPanel {
 	 * Create the panel.
 	 */
 	public YourRide() {
-		setLayout(new BorderLayout(0, 0));
+//		setLayout(new BorderLayout(0, 0));
 		
-		MapBean mapBean = new MapBean();
-		add(mapBean);
-		Properties shapeLayerProps = new Properties();
-	      shapeLayerProps.put("prettyName", "Political Solid");
-	      shapeLayerProps.put("lineColor", "000000");
-	      shapeLayerProps.put("fillColor", "BDDE83");
-	      shapeLayerProps.put("shapeFile", "resources/map/shape/dcwpo-browse.shp");
-	      shapeLayerProps.put("spatialIndex", "resources/map/shape/dcwpo-browse.ssx");
-	 
-	      ShapeLayer shapeLayer = new ShapeLayer();
-	      shapeLayer.setProperties(shapeLayerProps);
-	 
-	      // Add the political layer to the map
-	      mapBean.add(shapeLayer);
-		
+//		MapBean mapBean = new MapBean();
+//		add(mapBean);
+//		Properties shapeLayerProps = new Properties();
+//	      shapeLayerProps.put("prettyName", "Political Solid");
+//	      shapeLayerProps.put("lineColor", "000000");
+//	      shapeLayerProps.put("fillColor", "BDDE83");
+//	      shapeLayerProps.put("shapeFile", "resources/map/shape/dcwpo-browse.shp");
+//	      shapeLayerProps.put("spatialIndex", "resources/map/shape/dcwpo-browse.ssx");
+//	 
+//	      ShapeLayer shapeLayer = new ShapeLayer();
+//	      shapeLayer.setProperties(shapeLayerProps);
+//	 
+//	      // Add the political layer to the map
+//	      mapBean.add(shapeLayer);
+//		
 		
 	}
 }
