7083fe7edd8fd0f4364fc7679abe33bdc45fb741
pravalikha <pravalikhac1999@gmail.com>
2019-11-30 11:27:08 +0530

    Date and Time picker added

--- src/components/OfferARide.java
+++ src/components/OfferARide.java
@@ -1,26 +1,44 @@
 package components;
 
+
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import java.awt.SystemColor;
-import javax.swing.border.LineBorder;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.JSpinner.DateEditor;
+
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
+import javax.swing.SpinnerModel;
+import javax.swing.Timer;
+
+import com.toedter.calendar.JDateChooser;
+import com.toedter.components.JLocaleChooser;
 
 public class OfferARide extends JPanel {
-	private JTextField txtDestination;
-	private JTextField txtSource;
-	private JTextField txtTime;
+	private JTextField offerDest;
+	private JTextField offerSource;
 	private JTextField txtCar;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private String date;
+	private JDateChooser dateChooser;
+	private JSpinner spinner;
+	private JComponent editor;
 
 	/**
 	 * Create the panel.
@@ -28,77 +46,103 @@ public class OfferARide extends JPanel {
 	public OfferARide() {
 		setLayout(null);
 		
+		date = "";
+		
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		panel.setBackground(Color.RED);
-		panel.setBounds(-30, 0, 968, 557);
+		panel.setBounds(-30, 0, 856, 511);
 		add(panel);
-		
 		JPanel panel_3 = new JPanel();
 		panel_3.setLayout(null);
 		panel_3.setForeground(Color.WHITE);
 		panel_3.setBackground(Color.BLACK);
-		panel_3.setBounds(63, 36, 867, 483);
+		panel_3.setBounds(63, 36, 783, 464);
 		panel.add(panel_3);
 		
-		txtDestination = new JTextField();
-		txtDestination.setText("Destination");
-		txtDestination.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
-		txtDestination.setColumns(10);
-		txtDestination.setBounds(66, 34, 251, 46);
-		panel_3.add(txtDestination);
+		offerDest = new JTextField();
+		offerDest.setText("Destination");
+		offerDest.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+		offerDest.setColumns(10);
+		offerDest.setBounds(66, 43, 251, 28);
+		panel_3.add(offerDest);
 		
 		JLabel lblTo = new JLabel("To:");
 		lblTo.setForeground(Color.WHITE);
-		lblTo.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
-		lblTo.setBounds(15, 42, 131, 28);
+		lblTo.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		lblTo.setBounds(15, 42, 36, 28);
 		panel_3.add(lblTo);
 		
 		JLabel lblFrom = new JLabel("From:");
 		lblFrom.setForeground(Color.WHITE);
-		lblFrom.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
-		lblFrom.setBounds(454, 41, 67, 28);
+		lblFrom.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		lblFrom.setBounds(15, 82, 48, 20);
 		panel_3.add(lblFrom);
 		
-		txtSource = new JTextField();
-		txtSource.setText("Source");
-		txtSource.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
-		txtSource.setColumns(10);
-		txtSource.setBounds(536, 34, 251, 46);
-		panel_3.add(txtSource);
-		
-		JLabel lblScheduleTime = new JLabel("Schedule time:");
-		lblScheduleTime.setForeground(Color.WHITE);
-		lblScheduleTime.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
-		lblScheduleTime.setBounds(15, 114, 219, 28);
-		panel_3.add(lblScheduleTime);
-		
-		txtTime = new JTextField();
-		txtTime.setText("Time");
-		txtTime.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
-		txtTime.setColumns(10);
-		txtTime.setBounds(66, 148, 251, 46);
-		panel_3.add(txtTime);
+		offerSource = new JTextField();
+		offerSource.setText("Source");
+		offerSource.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+		offerSource.setColumns(10);
+		offerSource.setBounds(66, 82, 251, 28);
+		panel_3.add(offerSource);
 		
 		txtCar = new JTextField();
 		txtCar.setText("Car");
 		txtCar.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		txtCar.setColumns(10);
-		txtCar.setBounds(536, 155, 251, 46);
+		txtCar.setBounds(66, 121, 251, 28);
 		panel_3.add(txtCar);
 		
 		JLabel lblVehicle = new JLabel("Vehicle:");
 		lblVehicle.setForeground(Color.WHITE);
-		lblVehicle.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
-		lblVehicle.setBounds(445, 155, 76, 28);
+		lblVehicle.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		lblVehicle.setBounds(15, 121, 48, 20);
 		panel_3.add(lblVehicle);
 		
 		JButton btnOffer = new JButton("Offer");
 		btnOffer.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
 		btnOffer.setBounds(365, 226, 115, 29);
 		panel_3.add(btnOffer);
-		
+		DateFormat fmt = new SimpleDateFormat("yyyy/MM/dd");
+
+		btnOffer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					int trip = 12;
+					int capacity = 0;
+					int temp = 0;
+					date = fmt.format(dateChooser.getDate());
+					String time = ((DateEditor) editor).getFormat().format(spinner.getValue()).toString();
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vehiclepoolingdb", "root","");
+					Statement stmt = con.createStatement();
+					String sqlForVid = "Select Vehicle_id from vehicle_lic where V_user_id = '" + Login.userid.getText() + "'";
+					ResultSet rsForVid = stmt.executeQuery(sqlForVid);
+					while(rsForVid.next()){
+						temp = rsForVid.getInt("Vehicle_id");
+					}
+					
+					String sqlForSeats = "Select Capacity from vehicle_desc where Vehicle_id = '"+temp+"'";
+					ResultSet rsForSeats = stmt.executeQuery(sqlForSeats);
+					while(rsForSeats.next()){
+						capacity = rsForSeats.getInt("Capacity");
+					}
+					capacity -= 1;
+					String sql = "Insert into trip(Trip_id,T_date,T_time,Source,Destination,Avail_seats)" + "values(" + (trip++) 
+							+ ",'"+date+"','"+time+"','"+ offerSource.getText() +"','"+ offerDest.getText()+"',"+ capacity+ ")" ;
+					int rs = stmt.executeUpdate(sql);
+					if (rs>0)
+						JOptionPane.showMessageDialog(null, "Ride offered!");
+					else
+						JOptionPane.showMessageDialog(null, "Failed to offer a ride");
+					con.close();
+
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+		});
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(25, 271, 341, 181);
 		panel_3.add(panel_1);
@@ -166,6 +210,28 @@ public class OfferARide extends JPanel {
 		lblNewLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
 		lblNewLabel.setBounds(52, 16, 264, 44);
 		panel_2.add(lblNewLabel);
+
+		dateChooser = new JDateChooser();
+		dateChooser.setBounds(66, 159, 251, 28);
+		panel_3.add(dateChooser);
+		
+		SpinnerDateModel model = new SpinnerDateModel();
+		model.setCalendarField(Calendar.MINUTE);
+
+		spinner= new JSpinner();
+		spinner.setModel(model);
+		 editor = new JSpinner.DateEditor(spinner,"HH:mm");
+		 spinner.setEditor(editor);
+		 spinner.setBounds(66,199,188,45);
+		 panel_3.add(spinner);
+		 
+		 
+
+		JLabel lblDate = new JLabel("Date");
+		lblDate.setForeground(Color.WHITE);
+		lblDate.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		lblDate.setBounds(15, 159, 48, 20);
+		panel_3.add(lblDate);
 
 	}
 }
